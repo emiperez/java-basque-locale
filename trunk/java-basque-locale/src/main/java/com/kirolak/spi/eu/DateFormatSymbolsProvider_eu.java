a91bdebc61423cd1b89ee6c0b303be9c99cb2bdb
fiestaspuntonet <fiestaspuntonet@245dd94b-9246-0410-bb49-71228a181e99>
2008-02-20 09:39:15 +0000

    Initial import.

---
+++ trunk/java-basque-locale/src/main/java/com/kirolak/spi/eu/DateFormatSymbolsProvider_eu.java
@@ -0,0 +1,38 @@
+package com.kirolak.spi.eu;
+
+/**
+ * @author Emilio Perez
+ * http://www.kirolak.com
+ * http://www.jaiak.net
+ */
+
+import java.text.DateFormatSymbols;
+import java.text.spi.DateFormatSymbolsProvider;
+import java.util.Locale;
+
+public class DateFormatSymbolsProvider_eu extends DateFormatSymbolsProvider {
+
+	private Locale basque = new Locale("eu", "ES");
+	@Override
+	public DateFormatSymbols getInstance(Locale locale) {
+		if (locale.equals(basque)) {
+            DateFormatSymbols df = new DateFormatSymbols();
+            String[] newMonths = {"urtarrila", "otsaila", "martxoa", "apirila", "maiatza", "ekaina", "uztaila", "abuztua", "iraila","urria", "azaroa", "abendua"};
+            String[] newShortMonths = {"urt", "ots", "mar", "api", "mai", "eka", "uzt", "abu", "ira","urr", "aza", "abe"};
+            String[] newShortWeekdays = {"leh", "art", "azk", "oeg", "oir", "lar", "iga"};
+            String[] newWeekdays = {"astelehena", "asteartea", "asteazkena", "osteguna", "ostirala", "larunbata", "igandea"};
+            df.setMonths(newMonths);
+                        
+            df.setShortMonths(newShortMonths);
+            df.setShortWeekdays(newShortWeekdays);
+            df.setWeekdays(newWeekdays);
+        }
+        return null;
+	}
+
+	@Override
+	public Locale[] getAvailableLocales() {
+		return new Locale[] {basque};
+	}
+
+}
