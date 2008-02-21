b3d68d31cdb8cc66d0efc3e97e7cd86f3dbaed3c
fiestaspuntonet <fiestaspuntonet@245dd94b-9246-0410-bb49-71228a181e99>
2008-02-21 11:46:10 +0000

    Added FormatSymbols and LocaleName

--- trunk/java-basque-locale/src/main/java/com/kirolak/spi/eu/DateFormatSymbolsProvider_eu.java
+++ trunk/java-basque-locale/src/main/java/com/kirolak/spi/eu/DateFormatSymbolsProvider_eu.java
@@ -12,10 +12,10 @@ import java.util.Locale;
 
 public class DateFormatSymbolsProvider_eu extends DateFormatSymbolsProvider {
 
-	private Locale basque = new Locale("eu", "ES");
-	@Override
+	private Locale basque = new Locale("eu");
+
 	public DateFormatSymbols getInstance(Locale locale) {
-		if (locale.equals(basque)) {
+		if (locale.getLanguage().equals(basque.getLanguage())) {
             DateFormatSymbols df = new DateFormatSymbols();
             String[] newMonths = {"urtarrila", "otsaila", "martxoa", "apirila", "maiatza", "ekaina", "uztaila", "abuztua", "iraila","urria", "azaroa", "abendua"};
             String[] newShortMonths = {"urt", "ots", "mar", "api", "mai", "eka", "uzt", "abu", "ira","urr", "aza", "abe"};
@@ -26,11 +26,11 @@ public class DateFormatSymbolsProvider_eu extends DateFormatSymbolsProvider {
             df.setShortMonths(newShortMonths);
             df.setShortWeekdays(newShortWeekdays);
             df.setWeekdays(newWeekdays);
+            return df;
         }
         return null;
 	}
 
-	@Override
 	public Locale[] getAvailableLocales() {
 		return new Locale[] {basque};
 	}
